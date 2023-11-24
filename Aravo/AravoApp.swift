35a81e87a7600a989f10c0cb0565711671b905a2
juliasakakibara <juliasakakibara.o@gmail.com>
2023-11-24 10:13:15 -0300

    Initial Commit

---
+++ Aravo/AravoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AravoApp.swift
+//  Aravo
+//
+//  Created by Julia Sakakibara on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AravoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
